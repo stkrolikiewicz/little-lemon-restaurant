edd66d126aea98ed5b12defc5c0ee7f1139065d6
stachkrol <st.krolikiewicz@gmail.com>
2023-01-16 19:14:50 +0100

    Setting up semantic HTML components

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+
+const Main = () => {
+    return (
+        <main>
+            <HeroSection />
+        </main>
+    );
+};
+
+export default Main;
